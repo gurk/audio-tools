bbe37fffeb627caad738851a607d036c506ec95e
Graeme Urquhart <g.urkhart@gmail.com>
2022-03-14 00:47:23 +0000

    class based filtergraph

--- preview_postprocess.py
+++ preview_postprocess.py
@@ -20,28 +20,74 @@ def main(cli_args):
             create_preview(path)
 
 
-# compressor - filter settings (ffmpeg)
-def compressor(threshold=0.1, ratio=2, attack=5):
-    return f'acompressor=threshold={threshold}:ratio={ratio}:attack={attack}'
+class FilterBase:
+    name = ''
+    settings = {}
+
+    def __init__(self, **kwargs):
+        self.settings.update(kwargs)
+
+    def __str__(self):
+        # generate filter argument string
+        return self._make_filter(self.name, self.settings)
+
+    @staticmethod
+    def _make_filter(command: str, settings: dict) -> str:
+        params = [f'{k}={v}' for k, v in settings.items()]
+        return f'{command}={":".join(params)}'
 
 
-# fade out - filter settings (ffmpeg)
-def fade_out(start=1.98, duration=0.02):
-    return f'afade=out:st={start}:d={duration}'
+class Compressor(FilterBase):
+    name = 'acompressor'
+    settings = dict(
+        threshold=0.1,
+        ratio=2,
+        attack=5
+    )
+
+
+class FadeOut(FilterBase):
+    name = 'afade'
+    settings = dict(
+        type='out',
+        start_time=1.98,
+        duration=0.02
+    )
+
+
+class FilterInstanceError(Exception):
+    pass
+
+
+class FilterGraph:
+    def __init__(self, *filters):
+        for f in filters:
+            if not isinstance(f, FilterBase):
+                raise FilterInstanceError('Filter must be subclass instance of FilterBase.')
+        self._filters = filters
+
+    def __str__(self):
+        # generate filtergraph argument string
+        return ','.join([str(f) for f in self._filters])
 
 
 def create_preview(path):
     """Process audio file with ffmpeg - max duration, add a compressor, and a fade-out."""
 
     # build .ogg output file path
-    input_path, _ = os.path.splitext(path)
-    output_path   = f'{input_path}.ogg'
+    path_no_ext, _ = os.path.splitext(path)
+    output_path    = f'{path_no_ext}.ogg'
 
-    # ffmpeg command + args
+    filters = FilterGraph(
+        Compressor(ratio=8),
+        FadeOut()
+    )
+
+    # [ffmpeg binary, *arguments, output path]
     command = ['/usr/local/bin/ffmpeg',                # ffmpeg executable
                '-i', path,                             # input file
                '-ss', '0', '-t', '2',                  # max duration 2s
-               '-af', f'{compressor()},{fade_out()}',  # filtergraph - [compressor -> fade_out]
+               '-af', f'{filters}',                    # audio filtergraph
                '-c:a', 'libvorbis',                    # convert to ogg, with libvorbis codec
                output_path]
 
